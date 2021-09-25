321b6ba3ee0132432a92e05d3b506ccf70eb988d
Connor King <sigstackfault@gmail.com>
2021-09-25 15:27:03 -0400

    fix item names

--- carbon.js
+++ carbon.js
@@ -6,35 +6,35 @@ var CATEGORIES = {
     "image": "images/trans.jpeg",
     "items": [
       {"name": "took the bus", "points": 5},
-      {"work": "bought cap", "points": 3}
+      {"name": "bought cap", "points": 3}
     ]
   },
   food: {
     "image": "images/food.jpg",
     "items": [
       {"name": "ate tuna sushi","points": -15},
-      {"work": "short run for coffee","points": 2}
+      {"name": "short run for coffee","points": 2}
     ]
   },
   social: {
     "image": "images/social.jpg",
     "items": [
       {"name": "drank local beer", "points": 3},
-      {"work": "went for trip", "points": 4}
+      {"name": "went for trip", "points": 4}
     ]
   },
   housekeeping: {
     "image": "images/housekeeping.jpg",
     "items": [
       {"name": "use push mower", "points": 10},
-      {"work": "walkway fixed", "points": 7}
+      {"name": "walkway fixed", "points": 7}
     ]
   },
   travel: {
     "image": "images/travel.jpeg",
     "items": [
       {"name": "took train", "points": 15},
-      {"work": "electric scooter for saviour", "points": 12}
+      {"name": "electric scooter for saviour", "points": 12}
     ]
   }
 }
