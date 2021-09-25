43e6d8d3daf3ea904b938ff0d5ff0fc41484b12b
Connor King <sigstackfault@gmail.com>
2021-09-25 14:02:29 -0400

    apsidfmaopsdimf

--- carbon.js
+++ carbon.js
@@ -23,19 +23,20 @@ var CATEGORIES = {
   }
 }
 
-var game = $("#game")
-console.log(game)
-
-for(var cat in CATEGORIES){
-  var catName = cat.toUpperCase()
-  cat = CATEGORIES[cat];
-  console.log("parsing category", cat);
-  // given more time I would write this in vanilla js
-  game.append(
-    $("<div>").addClass("game-category").append(
-      $(`<img src='${cat.image}'></img>`),
-      $(`<div>${catName}</div>`).addClass("label"),
+function renderCategories(){
+  var game = $("#game")
+  for(var cat in CATEGORIES){
+    var catName = cat.toUpperCase()
+    cat = CATEGORIES[cat];
+    console.log("parsing category", cat);
+    // given more time I would write this in vanilla js
+    game.append(
+      $("<div>").addClass("game-category").append(
+        $(`<img src='${cat.image}'></img>`),
+        $(`<div>${catName}</div>`).addClass("label"),
+      )
     )
-  )
+  }
 }
 
+renderCategories();
